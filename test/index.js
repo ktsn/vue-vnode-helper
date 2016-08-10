3dc5967dc64728b3ee21c873280d2f6b0dba5df0
ktsn <ktsn55@gmail.com>
2016-08-10 14:42:22 +0900

    Add test for build-in helper generator

---
+++ test/index.js
@@ -0,0 +1,15 @@
+import assert from 'power-assert'
+import { elementNames } from '../src/config'
+import { helpers } from '../src/index'
+import { kebabToCamel } from '../src/utils'
+
+describe('build-in helpers', () => {
+  it('should expose helpers', () => {
+    const keys = Object.keys(helpers)
+
+    assert(elementNames.length + 1 === keys.length) // include tag helper
+    elementNames.forEach(name => {
+      assert(typeof helpers[kebabToCamel(name)] === 'function')
+    })
+  })
+})
