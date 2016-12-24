f1b501854073808fdca61907f474eda101d0bec1
ktsn <ktsn55@gmail.com>
2016-12-24 13:58:17 +0900

    Fix lint error

--- test/index.ts
+++ test/index.ts
@@ -7,7 +7,7 @@ describe('build-in helpers', () => {
   it('should expose helpers', () => {
     const keys = Object.keys(helpers)
 
-    assert(elementNames.length === keys.length) 
+    assert(elementNames.length === keys.length)
     elementNames.forEach(name => {
       assert(typeof helpers[kebabToCamel(name)] === 'function')
     })
